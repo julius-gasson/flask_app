981a0e49e88c968900fe3f6e3b2bbe82e59da498
Julius Gasson <jg2023@ic.ac.uk>
2023-10-23 15:29:21 +0100

    Added yml file

--- api/app.py
+++ api/app.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
-
-        
-

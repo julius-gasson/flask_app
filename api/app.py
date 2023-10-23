300e6f2db96f0318080a91ec3bc87d1515794ee1
Julius Gasson <jg2023@ic.ac.uk>
2023-10-23 16:58:07 +0100

    Tried to fix it again

--- api/app.py
+++ api/app.py
@@ -7,11 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
 
 def process_query(input):
     if input == "dinosaurs":
@@ -19,7 +21,8 @@ def process_query(input):
     if input == "asteroids":
         return "Unknown"
 
-@app.route("/query", methods=["GET"])
+
+    @app.route("/query", methods=["GET"])
 def query():
     query_param = request.args.get('q')
     return process_query(query_param)

685c0700aec60f96da9f905bc0263ee1244e6932
Julius Gasson <jg2023@ic.ac.uk>
2023-10-23 16:51:38 +0100

    Added unittest

--- api/app.py
+++ api/app.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 
-
-
 def hello_world():
     return render_template("index.html")
 
@@ -16,3 +14,14 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+def process_query(input):
+    if input == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    if input == "asteroids":
+        return "Unknown"
+
+@app.route("/query", methods=["GET"])
+def query():
+    query_param = request.args.get('q')
+    return process_query(query_param)
